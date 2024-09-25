b7cac3f38f880a4c32cbd9b14295a46800d25e4a
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-25 19:57:08 +0530

    Delete 1960-check-if-the-sentence-is-pangram directory

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean checkIfPangram(String sentence) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        for(char ch:sentence.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        return map.size()==26?true:false;
-    }
-}
