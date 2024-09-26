68115e578cc899b481675d945ee8f5d408cca2b9
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-26 14:38:27 +0530

    Time: 1 ms (100.00%) | Memory: 133.4 MB (84.35%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,24 +1,16 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int prefix[]=new int[nums.length];
-        int min=nums[0];
-        prefix[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            prefix[i]=min;
-            min=Math.min(min,nums[i]);
+        if (nums == null || nums.length < 3) {
+            return false;
         }
-        for(int x:prefix)System.out.print(x+" ");
-        int max=nums[nums.length-1];
-        int suffix[]=new int[nums.length];
-        suffix[nums.length-1]=max;
-        for(int i=nums.length-2;i>=0;i--){
-            suffix[i]=max;
-            max=Math.max(max,nums[i]);
-        }
-        System.out.println();
-        for(int x:suffix)System.out.print(x+" ");
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>prefix[i] && nums[i]<suffix[i] && nums[i]!=prefix[i] && nums[i]!=suffix[i]){
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num <= a) {
+                a = num;
+            } else if (num <= b) {
+                b = num;
+            } else {
                 return true;
             }
         }
