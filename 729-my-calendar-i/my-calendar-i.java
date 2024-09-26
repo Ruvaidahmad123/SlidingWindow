e86381483988df1cfb376be3f0332c9744eb911e
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-26 21:58:51 +0530

    Time: 21 ms (68.37%) | Memory: 45.5 MB (42.73%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.java
+++ 729-my-calendar-i/my-calendar-i.java
@@ -1,20 +1,31 @@
 class MyCalendar {
-    // List to store the booked intervals
-    List<int[]> calendar;
+
+    // TreeSet to store the booked intervals
+    TreeSet<int[]> st;
+
     public MyCalendar() {
-        // Initialize the list
-        calendar = new ArrayList<>();
+        // Initialize the TreeSet with a custom comparator
+        st = new TreeSet<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
     }
+
     public boolean book(int start, int end) {
-        // Check for overlap with every existing booking
-        for (int[] curr : calendar) {
-            // If there is overlap, return false
-            if (!(end <= curr[0] || start >= curr[1])) {
-                return false;
-            }
+        // Find the first event that starts after or at the same time of {start, end}
+        int[] event = new int[]{start, end};
+        int[] next = st.ceiling(event); // O(logN)
+
+        // Check if the current event overlaps with the next event
+        if (next != null && next[0] < end) {
+            return false;
         }
-        // If no overlap, add the booking to the calendar
-        calendar.add(new int[]{start, end});
+
+        // Check if the current event overlaps with the previous event
+        int[] prev = st.floor(event);
+        if (prev != null && start < prev[1]) {
+            return false;
+        }
+
+        // If no overlap, add the booking to the set
+        st.add(new int[]{start, end});
         return true;
     }
 }
