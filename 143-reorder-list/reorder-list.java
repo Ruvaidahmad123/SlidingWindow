b8ab1289d2a245e344700642002d6fae360ec9c4
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-28 13:07:07 +0530

    Time: 4 ms (15.73%) | Memory: 48 MB (58.81%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -19,7 +19,7 @@ class Solution {
         }
         // System.out.println(count);
         Stack<ListNode>st=new Stack<>();
-        if(count%2==0){
+        
             ListNode newtemp=head;
             int tempcount=0;
             while(tempcount<count/2){
@@ -43,31 +43,6 @@ class Solution {
             }
             newtemp.next=null;
             // newte
-        }
-        else if(count%2==1){
-            ListNode newtemp=head;
-            int tempcount=0;
-            while(tempcount<count/2){
-                tempcount++;
-                newtemp=newtemp.next;
-            }
-            // System.out.println(newtemp.val);
-            while(newtemp!=null){
-                st.push(newtemp);
-                newtemp=newtemp.next;
-            }
-            newtemp=head;
-            int a=0;
-            while(a<count/2){
-                a++;
-                ListNode nexttemp=newtemp.next;
-                ListNode next1=st.pop();
-                newtemp.next=next1;
-                next1.next=nexttemp;
-                newtemp=nexttemp;
-            }
-            newtemp.next=null;
-            // newte
-        }
+        
     }
 }
